9a18d2adf18cc8a925a68842ef35d34711f63a64
libelo <libelo28@gmail.com>
2015-07-16 22:44:27 -0700

    logging

--- app/mass_calculator/mass_calculator.py
+++ app/mass_calculator/mass_calculator.py
@@ -2,8 +2,17 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, db
 import csv
 from os import path
+import logging
 
-PERIODIC_TABLE = path.join(path.dirname(__file__),'periodic_table.csv')
+PERIODIC_TABLE = path.join(path.dirname(__file__), 'periodic_table.csv')
+LOG_FILE = path.join(path.dirname(__file__), 'mass_calculator.log')
+
+logger = logging.getLogger('mass calculator logger')
+formatter = logging.Formatter('%(levelname)s %(asctime)s %(message)s')
+fileHandler = logging.FileHandler(LOG_FILE)
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)
 
 @app.route('/mass_calculator')
 def mass_calculator():
@@ -13,6 +22,7 @@ def mass_calculator():
 def calculate_mass():
 	# request.form.getlist('elements[]')
 	data = request.get_json(force=True)
+	logger.info("mass_calculator_input - " + str(data))
 
 	def molar_mass(element):
 		with open( PERIODIC_TABLE ) as table:
@@ -25,6 +35,7 @@ def calculate_mass():
 	for element in formula:
 		reference_atoms = float(data['reference_mass']) / molar_mass(data['reference_element']) / float(formula[data['reference_element']])
 		result[element] =  reference_atoms * float(formula[element]) * molar_mass(element)
-
+	
+	logger.info("mass_calculator_output - " + str(result))
 	return jsonify(result)
 
